eb7666e7b6b9e16d0785f715c93be811cd4b3bc7
guanw <guanw0826@gmail.com>
2020-01-31 09:02:23 -0500

    init

---
+++ client/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/guanw/kubernetes-service-discovery-grpc/IDL"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8082"
+)
+
+func main() {
+	// Set up a connection to the server.
+	var url = "localhost"
+	conn, err := grpc.Dial(url+port, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	for {
+		// Contact the server and print out its response.
+		name := "Jude"
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.GetMessage())
+
+		time.Sleep(5 * time.Second)
+	}
+}
